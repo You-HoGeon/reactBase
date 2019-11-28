c65cda7ee069d2daea3244bf185c70fafd8196df
YouHoGeon <idog@youhogeon.com>
2019-11-28 21:59:53 +0900

    Add Intro Component

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Intro.scss';
 import imgReact from './react.png';
 
-class Intro extends React.Component {
+/* class Intro extends React.Component {
   constructor() {
     super();
     this.state = { title: 'test' };
@@ -12,12 +12,19 @@ class Intro extends React.Component {
     const { title } = this.state;
     return (
       <div id="App">
-        Hello Worlds
         <img src={imgReact} alt="Intro" />
         {title}
       </div>
     );
   }
-}
+} */
+
+const Intro = () => (
+  <div id="intro">
+    <img src={imgReact} alt="Intro" id="img_intro" />
+    <h1 id="header_intro">React Base</h1>
+    <h2 id="description_intro">Design your react app.</h2>
+  </div>
+);
 
 export default Intro;
